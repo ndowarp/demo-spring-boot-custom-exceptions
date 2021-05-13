788e45748fa4c832a0c33f051009ed83e2674914
ndowarp <nicolas961945@gmail.com>
2021-05-13 13:41:46 -0500

    Erase unnecessary imports
    Erase unnecessary imports

--- src/main/java/com/example/demospringbootcustomexceptions/exception/ApiExceptionHandler.java
+++ src/main/java/com/example/demospringbootcustomexceptions/exception/ApiExceptionHandler.java
@@ -5,7 +5,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import java.time.ZoneId;
 import java.time.ZonedDateTime;
 
 @ControllerAdvice
